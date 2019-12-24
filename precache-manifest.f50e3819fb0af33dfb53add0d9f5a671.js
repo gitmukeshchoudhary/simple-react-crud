6483673dfec935f3a603727333776148d30b82dd
mukesh choudhary <mukachoudhary@gmail.com>
2019-12-24 11:06:15 +0530

    Updates

---
+++ precache-manifest.f50e3819fb0af33dfb53add0d9f5a671.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f206a2514c3618d42762e250c18d620a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb8351b2d709dbf21811",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6446623877555f8949ac",
+    "url": "/static/js/2.40a41ba8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.40a41ba8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb8351b2d709dbf21811",
+    "url": "/static/js/main.957d25fb.chunk.js"
+  },
+  {
+    "revision": "b01418a39d2e87564827",
+    "url": "/static/js/runtime-main.d1217d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
